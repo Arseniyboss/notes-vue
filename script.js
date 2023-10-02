c9e3c7f069e141e6deec8f6273d7df6a44297fd3
Arseniy Zakharchenko <arseniyzakharchenko@gmail.com>
2023-10-02 17:59:52 +0200

    add functionality to add notes

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const { createApp } = Vue
 
-const app = createApp({})
+const app = createApp({
+  data() {
+    return {
+      notes: [],
+      title: '',
+      note: '',
+    }
+  },
+  methods: {
+    addNote() {
+      const { title, note } = this
+      if (!title && !note) return
+      this.notes.push({ id: crypto.randomUUID(), title, note })
+      this.title = ''
+      this.note = ''
+      this.$refs.titleInput.focus()
+    },
+  },
+})
 
 app.mount('#app')
